8179dbbc9aaf971d2109e5515585d374fd6d27c3
Joy Huang <joyhuangg@gmail.com>
2018-07-18 02:55:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   let result = ""
-  if string === string.toLowerCase()
+  if (string === string.toLowerCase())
     result = "I can\'t hear you!"
-  else if string === string.toUpperCase()
+  else if (string === string.toUpperCase())
     result = "YES INDEED!"
-  else if string === "I love you, Grandma."
+  else if (string === "I love you, Grandma.")
     result = "I love you, too."
   return result
 }
